e94a067eaa5008d41c527c69148f419eba276ba4
gustavo-nt <gustavont.dev@gmail.com>
2021-11-14 23:10:59 -0300

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs';
+import { App } from './App';
+
+createServer({
+    models: {
+        transaction: Model,
+    },
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Desenvolvimento de Website',
+                    type: "deposit",
+                    category: 'Desenvolvimento',
+                    amount: '12.000',
+                    createdAt: new Date('2021-02-12 09:00:00')
+                }
+            ]
+        });
+    },
+    routes() {
+        this.namespace = 'api';
+
+        this.get('/transactions', () => {
+            return this.schema.all('transaction');
+        });
+
+        this.post('/transactions', (schema, request) => {
+            return schema.create('transaction', JSON.parse(request.requestBody));
+        });
+    }
+}); 
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
